d31cfc48e55eec9818b933198d47631f9db6a3ed
blaz96m <blovric966@gmail.com>
2025-03-08 18:30:48 +0100

    remove un-needed files

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { Provider } from "react-redux";
-
-createRoot(document.getElementById("root")!).render(<App />);
