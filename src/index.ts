a480559abd8631010a5a9ad66df9ed05a71ec76a
blaz96m <blovric966@gmail.com>
2025-03-17 00:03:36 +0100

    move font awesome inject fix to index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Prevent FontAwesome from injecting its own CSS
+
 export { Select } from "src/Select/main";
 export { useSelect, useSelectAsync } from "src/Select/hooks";
 export * from "src/Select/types/selectComponentTypes";
